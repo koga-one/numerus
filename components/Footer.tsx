e1be51c409ca6c6a0fb6baf9a50ec1f0d6438b71
André Koga <social@koga.one>
2022-06-24 18:52:39 -0300

    solving bugs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,13 +10,23 @@ const Footer = () => {
       <div className="order-1 flex flex-col place-content-center items-center lg:order-2">
         <Link href="/">
           <a>
-            <Image src="/favicon.svg" width="30px" height="30px" />
+            <Image
+              src="/favicon.svg"
+              alt="The logo of the website"
+              width="30px"
+              height="30px"
+            />
           </a>
         </Link>
-        <p className="text-xs">numbers are beautiful, aren't they?</p>
+        <p className="text-xs">{"numbers are beautiful, aren't they?"}</p>
       </div>
       <div className="order-2 flex justify-center gap-8 text-center lg:order-3 lg:justify-start">
-        <a target="_blank" href="https://koga.one" className="underline">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://koga.one"
+          className="underline"
+        >
           Check out my blog
         </a>
       </div>
